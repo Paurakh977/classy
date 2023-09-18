ce9110c23fda47244003601467df161252215396
“Paurakh977” <pandeypaurakhraj@gmail.com>
2023-09-18 23:19:43 +0545

    teej ko din ko final wit settings wala

--- Classy/settings.py
+++ Classy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h-lle9*9=ih&sbef9t3pie2k8px7om%2_%s#n35s0$q-x*=uj3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.101','127.0.0.1']
+ALLOWED_HOSTS = ['192.168.0.104','127.0.0.1']
 
 
 # Application definition
